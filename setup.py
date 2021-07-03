2da75e1aed9525250802d09f24baa220164ca650
Tom <elunico@users.noreply.github.com>
2021-07-03 00:50:35 -0400

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tecoradors-elunico",
-    version="2.0.0",
+    version="2.0.1",
     author="Thomas Povinelli",
-    author_email="author@example.com",
+    author_email="tompov227@gmail.com",
     description="A small collection of decorators I like to use often",
     long_description=long_description,
     long_description_content_type="text/markdown",
 include_package_data=True,
-    url="https://gist.github.com/elunico/bde1125c1c31fae18f64a6437f2fbe03",
+    url="https://github.com/elunico/tecoradors",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
