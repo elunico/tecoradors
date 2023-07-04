663e1e8b63c81c1e2f683645241af98ce9c43447
Thomas Povinelli <elunico@users.noreply.github.com>
2023-07-04 14:08:32 -0400

    fixed __init__.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="tecoradors-elunico",
-    version="5.2.1",
+    version="5.2.2",
     author="Thomas Povinelli",
     author_email="tompov227@gmail.com",
     description="A small collection of decorators I like to use often",
--- tecoradors/__init__.py
+++ tecoradors/__init__.py
@@ -1,12 +1,16 @@
 from tecoradors.tecoradors import (
     accepts, dataclass, deprecated, equatable, final, freeze, FrozenClassError, hashable, json_serializable, log, Self,
-    spread, squash, stringable, synchronized, timed, count_calls, returns, interruptable, lazy, precompute, PrecomputeStorage, NoSuchValue
+    spread, squash, stringable, synchronized, timed, count_calls, returns, interruptable, lazy, precompute, PrecomputeStorage, NoSuchValue,
+    builder, tattle, TattleOptions
 )
 
 __all__ = [
     "accepts",
     "json_serializable",
     "spread",
+    'builder',
+    'tattle',
+    'TattleOptions',
     "timed",
     "squash",
     "stringable",
